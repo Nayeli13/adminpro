615e7aeaa6f89a33d58433ab5caa2fe1325a02c4
Nayeli Gonzales <nayeligonzales@Kurama.local>
2019-09-06 09:43:55 -0400

    Modificaciones en los modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { App_Routes } from './routes';
+import { PagesModule } from './pages/pages.module';
+
 
 App_Routes
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LogingComponent } from './loging/loging.component';
-import { NopagefoundComponent } from './nopagefound/nopagefound.component';
-import { DeshboardComponent } from './pages/deshboard/deshboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './loging/register.component';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+
+
 
 
 
@@ -23,21 +21,14 @@ import { RegisterComponent } from './loging/register.component';
   declarations: [
     AppComponent,
     LogingComponent,
-    NopagefoundComponent,
-    DeshboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
     RegisterComponent,
-    
+    NopagefoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    App_Routes
+    App_Routes,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
